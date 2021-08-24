588287557a49433ad845bd5eda531eaf6110f83b
Arun R S <arun.rs@iiitmk.ac.in>
2021-08-24 13:16:40 +0530

    Create templates for manufacturer - Add Car

---
+++ AutoApp/public/javascripts/main.js
@@ -0,0 +1,71 @@
+//swal Basic 
+function swalBasic(data) {
+    swal.fire({
+        // toast: true,
+        icon: `${data.icon}`,
+        title: `${data.title}`,
+        animation: true,
+        position: 'center',
+        showConfirmButton: true,
+        footer: `${data.footer}`,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', swal.stopTimer)
+            toast.addEventListener('mouseleave', swal.resumeTimer)
+        }
+    })
+}
+
+
+// manwrite function
+function ManWriteData() {
+    const vin = document.getElementById('vinNumber').value;
+    const make = document.getElementById('carMake').value;
+    const model = document.getElementById('carModel').value;
+    const color = document.getElementById('carColour').value;
+    const dom = document.getElementById('dom').value;
+    const flag = document.getElementById('manName').value;
+    if (vin.length==0||make.length==0||model.length==0||color.length==0||dom.length==0||flag.length==0) {
+        const data = {
+            title: "You might have missed something",
+            footer: "Enter all mandatory fields to add a new car",
+            icon: "warning"
+        }
+    swalBasic(data);
+    } else {
+        fetch('/manuwrite',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',              
+            },
+            body: JSON.stringify({VinNumb: vin, CarMake: make, CarModel: model, CarColor: color, DOM: dom, CarFlag: flag})
+        })
+        .then(function(response){
+            if(response.status == 200) {
+                const data = {
+                    title: "Success",
+                    footer: "Added a new car",
+                    icon: "success"
+                }
+                swalBasic(data);
+            } else {
+                const data = {
+                    title: `Car with Vin Number ${vin} already exists`,
+                    footer: "Vin Number must be unique",
+                    icon: "error"
+                }
+                swalBasic(data);
+            }
+
+        })
+        .catch(function(error){
+            const data = {
+                title: "Error in processing Request",
+                footer: "Something went wrong !",
+                icon: "error"
+            }
+            swalBasic(data);
+        })        
+    }    
+}
--- AutoApp/routes/index.js
+++ AutoApp/routes/index.js
@@ -6,4 +6,37 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Manufacturer Dashboard' });
 });
 
+/* GET Manufacturer page. */
+router.get('/manufacturer', function(req, res, next) {
+  res.render('manufacturer', { title: 'Manufacturer Dashboard' });
+});
+
+/* Manuwrite */ 
+router.post('/manuwrite', function(req, res){
+  const vin = req.body.VinNumb;
+  const make = req.body.CarMake;
+  const model = req.body.CarModel;
+  const color = req.body.CarColor;
+  const DOM = req.body.DOM;
+  const flag = req.body.CarFlag;
+
+  let ManufacturerClient = new clientApplication();
+  ManufacturerClient.generateAndSubmitTxn(
+    "manufacturer",
+    "Admin",
+    "autochannel", 
+    "KBA-Automobile",
+    "CarContract",
+    "createCar",
+    vin,make,model,color,DOM,flag
+  ).then(message => {
+    console.log("Message is $$$$",message)
+    res.status(200).send({message: "Added Car"})
+  })
+  .catch(error =>{
+    console.log("Some error Occured $$$$###", error)
+    res.status(500).send({error:`Failed to Add`,message:`${error}`})
+  });
+})
+
 module.exports = router;
