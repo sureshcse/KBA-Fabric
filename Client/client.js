4f13328bdc8d9b2ed1cd835d997708c005206473
sandeepkba <sandeep.krishnan@iiitmk.ac.in>
2021-08-24 12:18:16 +0530

    Class of Transaction 1

--- Client/client.js
+++ Client/client.js
@@ -26,32 +26,7 @@ class clientApplication {
            gateway.disconnect()
        }
 
-   }
-
-   async generateAndEvaluateTxn(role,identityLabel,channelName,chaincodeName,contractName,txnName,...args){
-    let gateway = new Gateway();
-    try {
-        this.Profile = profile[role.toLowerCase()]
-        const ccpPath = path.resolve(this.Profile["CP"])
-        const ccp = JSON.parse(fs.readFileSync(ccpPath,'utf-8'))
-        let wallet = await Wallets.newFileSystemWallet(this.Profile["Wallet"])
-        await gateway.connect(ccp,{wallet, identity: identityLabel,discovery: {enabled:true, asLocalhost:true}})
-        let channel = await gateway.getNetwork(channelName)
-        let contract = await channel.getContract(chaincodeName,contractName)
-        let result = await contract.evaluateTransaction(txnName,...args)
-        console.log(result)
-        return result
-
-     } catch (error) {
-         console.log("Error occured",error)
-        
-    } finally {
-        console.log("Disconnect from the gateway.")
-        gateway.disconnect()
-    }
-
-}
-   
+   }   
 
 }
 
